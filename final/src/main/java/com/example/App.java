ed661f4d7273b4ef02e189b841fcd61ffa49c4fa
sclark220 <113330749+sclark220@users.noreply.github.com>
2024-10-02 22:50:10 -0400

    Add files via upload

--- final/src/main/java/com/example/App.java
+++ final/src/main/java/com/example/App.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -16,42 +20,47 @@ import javafx.stage.Stage;
 
 public class App extends Application
 {
+    Pane mainMenuPane = new Pane();
     Pane originalPane = new Pane();
 
     private Sprite player = new Sprite(615, 650, 50, 50, "Player", Color.PURPLE);
 
-    public Parent drawStuff()
+    private List<Sprite> sprites()
     {
-        originalPane.setPrefSize(1280, 720);
 
-        Label headerText = new Label("This program will be a game!");
-
-        Font titleFont = Font.font("Times", FontWeight.EXTRA_BOLD, 42); // set title font and size
-        Font otherFont = Font.font("Times", FontWeight.EXTRA_BOLD, 15); // set font and size
-
-        GridPane.setConstraints(headerText, 1, 0);
-        headerText.setFont(titleFont);
-
-        originalPane.getChildren().addAll(headerText, player);
-
-        AnimationTimer timer = new AnimationTimer()
-        {
-            public void handle(long now)
-            {
-                update();
-            }
-        };
-
-        timer.start();
-
-        nextLevel();
-
-        return originalPane;
+        return originalPane.getChildren().stream().map(n -> (Sprite)n).collect(Collectors.toList());
     }
 
     private void update()
     {
+        // sprites().forEach(s -> 
+        // {
+        //     switch (s.type)
+        //     {
+        //         case "enemyattacksprite":
+        //             s.moveDown();
 
+        //             if(s.getBoundsInParent().intersects(player.getBoundsInParent())) // check for collision
+        //             {
+        //                 player.isDead = true;
+        //                 s.isDead = true;
+        //                 System.out.println("Player is dead");
+        //             }
+        //             break;
+            
+        //         case "playerattacksprite":
+        //             s.moveUp();
+
+        //             sprites().stream().filter(e - > e.type.equals("enemy")).forEach(enemy ->
+        //             {
+        //                 if (s.getBoundsInParent().intersects(enemy.getBoundsInParent()))
+        //                 {
+                            
+        //                 }
+        //             });
+        //             break;
+        //     }
+        // });
     }
 
     private void nextLevel()
@@ -78,11 +87,48 @@ public class App extends Application
     @Override // Override the start method in the Application class
     public void start(Stage primaryStage)
     {
-        Scene scene = new Scene(drawStuff());
+        Scene mainMenuScene = new Scene(mainMenuPane);
+        Scene gameScene = new Scene(originalPane);
 
-        scene.setOnKeyPressed(e -> 
+        Font titleFont = Font.font("Times", FontWeight.EXTRA_BOLD, 42); // set title font and size
+        Font otherFont = Font.font("Times", FontWeight.EXTRA_BOLD, 15); // set other font and size
+        
+     // -------------------- Main Menu --------------------------------------------------------------------------------------------//  
+        mainMenuPane.setPrefSize(1280, 720);
+
+        Label headerText = new Label("This program will be a game!");
+        headerText.layoutXProperty().bind(mainMenuPane.widthProperty().subtract(headerText.widthProperty()).divide(2)); // set to be the center of the screen
+        headerText.setFont(titleFont);
+
+        Button playGameButton = new Button();
+        playGameButton.setText("PLAY!");
+        playGameButton.setPrefSize(100, 50);
+        playGameButton.layoutXProperty().bind(mainMenuPane.widthProperty().subtract(playGameButton.widthProperty()).divide(2));
+        playGameButton.layoutYProperty().bind(mainMenuPane.heightProperty().subtract(playGameButton.heightProperty()).divide(2));
+        playGameButton.setOnAction(e -> primaryStage.setScene(gameScene));
+
+        mainMenuPane.getChildren().addAll(headerText, playGameButton);
+     // ---------------------------------------------------------------------------------------------------------------------------//
+     // ------------------------ Game----------------------------------------------------------------------------------------------// 
+        originalPane.setPrefSize(1280, 720);
+
+        originalPane.getChildren().addAll(player);
+
+        AnimationTimer timer = new AnimationTimer()
         {
-            switch (e.getCode()) 
+            public void handle(long now)
+            {
+                update();
+            }
+        };
+
+        timer.start();
+
+        nextLevel();
+     // -------------------------------------------------------------------------------------------------------------------------//
+        gameScene.setOnKeyPressed(e -> 
+        {
+            switch (e.getCode())
             {
                 case LEFT:
                 case A:
@@ -96,7 +142,6 @@ public class App extends Application
 
                 case UP:
                 case W:
-                case SPACE:
                     player.moveUp();
                     break;
 
@@ -104,10 +149,13 @@ public class App extends Application
                 case S:
                     player.moveDown();
                     break;
+                case SPACE:
+                    attack(player);
+                    break;
             }
         });
 
-        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.setScene(mainMenuScene); // Place the scene in the stage
         primaryStage.setTitle("Game"); // Set the stage title
         primaryStage.show(); // Display the stage
     }
@@ -127,19 +175,19 @@ public class App extends Application
 
         void moveLeft()
         {
-            setTranslateX(getTranslateX() - 5);
+            setTranslateX(getTranslateX() - 7);
         }
         void moveRight()
         {
-            setTranslateX(getTranslateX() + 5);
+            setTranslateX(getTranslateX() + 7);
         }
         void moveUp()
         {
-            setTranslateY(getTranslateY() - 5);
+            setTranslateY(getTranslateY() - 7);
         }
         void moveDown()
         {
-            setTranslateY(getTranslateY() + 5);
+            setTranslateY(getTranslateY() + 7);
         }
     }
 
